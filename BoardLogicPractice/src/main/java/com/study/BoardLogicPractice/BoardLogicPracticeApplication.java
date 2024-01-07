aae01880689aae48d30632b077de67f4f30977cd
Moonjunwoong <117076854+junwoong2@users.noreply.github.com>
2024-01-07 17:16:46 +0900

    Add files via upload

---
+++ BoardLogicPractice/src/main/java/com/study/BoardLogicPractice/BoardLogicPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.study.BoardLogicPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardLogicPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardLogicPracticeApplication.class, args);
+	}
+
+}
